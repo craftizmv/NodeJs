be48c5c33d5dfc7933ae61ae3aaf8a24302ab9bd
Mayank Verma <mayank.verma@dish.com>
2020-07-05 16:20:34 +0530

    Refined server.js file with more methods.

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,5 +1,9 @@
+
+
 const express = require("express");
 
+
+//Binding the express modile to this app variable.
 const app = express();
 
 
@@ -10,11 +14,19 @@ app.get("/", function(req, res) {
 })
 
 app.get("/contact", function(req, res) {
-  //console.log(req)
-  console.log(res)
+  console.log(req)
+  // console.log(res)
   res.send("Contact me at : mayank.verma12389@gmail.com")
 })
 
+
+app.get("/about", function(req, res) {
+  console.log(req)
+  // console.log(res)
+  res.send("My Name is Mayank, I love to learn new things and focus bit on my health.")
+})
+
+//Listen on a specific port. - Barebone of express server
 app.listen(3000, function() {
   console.log("Server started on port 3000")
 });
